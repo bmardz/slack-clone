4e17267e446ceb03a79e4b0db7f42680705b29aa
Bryce Mardon <brycemardon@Bryces-MBP.modem>
2020-08-25 18:03:10 +1000

    updated files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 
 function App() {
   return (
     // BEM naming convention
     <div className="App">
-      <h1>Slack Clone 💰</h1>
-
-      {/* Header */}
-      <Header />
-      {/* Sidebar */}
-      {/* React-Router -> Chat screen*/}
-
+        <Header />
+      <div className="app__body">
+        <Sidebar />
+            {/* Sidebar */}
+            {/* React-Router -> Chat screen*/}
+      </div>
     </div>
   );
 }
